c6b57650cdf36c697b0ca0112c414cf2bbab3c3e
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-02-11 11:43:50 +0530

    Fix weight conversion calculations for units

--- _to_convert_weight.py
+++ _to_convert_weight.py
@@ -1,10 +1,10 @@
 weight=int(input("Enter your weight: "))
 unit=input("Enter your unit(Kg or L): ")
 if unit in ["l"," L"]:
-    weight*=0.453592
+    weight*=0.45
     print(f"Your weight is {weight} Kg")
 elif unit in ["kg"," Kg","KG","kG"]:
-    weight*=2.20462
+    weight/=0.45
     print(f"your weight is {weight} L")
 else :
     print("Invalid unit")
