b6b14b550fcb9bfe8b2a5e2756852887d2fe09ad
ARVIND SINGH <arvind05singh9617@gmail.com>
2026-02-12 17:06:20 +0530

    Add digit to word converter for phone numbers

---
+++ digit_to_word_converter.py
@@ -0,0 +1,4 @@
+phone_number=input("Enter Number: ")
+numbers={1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",0:"Zero"}
+for digit in phone_number:
+    print(numbers[int(digit)],end="  ")
